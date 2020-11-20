3136f2fc55c3fdbc631a78e3ffea808ef0bcd241
bardanovk <bardanovk@gmail.com>
2020-11-20 03:59:29 +0300

    add second input token and modify verifyF2B for this

--- middleware/verifyF2B.js
+++ middleware/verifyF2B.js
@@ -1,12 +1,25 @@
 const speakeasy = require('speakeasy')
 const config = require('../config/tokens')
 
-const verify = (token) => {
-    return speakeasy.totp.verify({
-        secret: config.secret,
-        encoding: 'base32',
-        token: token
-    })
+const verify = (token1, token2) => {
+    try {
+        if (
+            speakeasy.totp.verify({
+                secret: config.auth1,
+                encoding: 'base32',
+                token: token1
+            }) &&
+            speakeasy.totp.verify({
+                secret: config.auth2,
+                encoding: 'base32',
+                token: token2
+            })) {
+            return true
+        }
+    } catch (error) {
+        console.log('VERIFY COOKIE ERROR', error)
+        return false
+    }
 }
 
 module.exports = verify

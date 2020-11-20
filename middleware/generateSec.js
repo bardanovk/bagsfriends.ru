c2f4f6958a1b346983a69ef75932fd34bdd7fa03
bardanovk <bardanovk@gmail.com>
2020-11-20 04:00:15 +0300

    rebuild generate secret tokens

--- middleware/generateSec.js
+++ middleware/generateSec.js
@@ -2,12 +2,27 @@ const speakeasy = require('speakeasy')
 const fs = require('fs')
 const path = require('path')
 
+try {
+    const auth1 = speakeasy.generateSecret({
+        name: 'bagsfriends1'
+    })
+    const auth2 = speakeasy.generateSecret({
+        name: 'bagsfriends2'
+    })
 
-const auth = speakeasy.generateSecret({
-    name: 'bagsfriends'
-})
+    const cookie = speakeasy.generateSecret()
+    const bcrypt = speakeasy.generateSecret()
+
+    console.log('auth1:', auth1)
+    console.log('auth2:', auth2)
+    console.log('cookie:', cookie)
+    console.log('bcrypt:', bcrypt)
 
 
-
-console.log(auth)
-fs.writeFileSync(path.join(__dirname, '../config/tokens.js'), `module.exports = {secret: '${auth.base32}}`)
+    fs.writeFileSync(path.join(__dirname, '../config/tokens.js'), `module.exports = {\nauth1: '${auth1.base32}', \nauth2: '${auth2.base32}', \ncookie: '${cookie.ascii}', \nbcryptSalt: '${bcrypt.ascii}'\n}`)
+} catch (e) {
+    console.log('GENERATE SECRET KEYS ERROR', e)
+}
+/*
+cookie: 'GBLDAQURS6WZBG5TUGXRZGY3DOUDRGUYQCBLNNEQPTMGBHV2QLTG',
+    bcryptSalt: 'nfdjab2214!199@@#!'*/

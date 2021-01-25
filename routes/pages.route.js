4aa18675675f303fb9acbbb007989a29ef549617
bardanovk <bardanovk@gmail.com>
2021-01-25 06:17:07 +0300

    basket fix

--- routes/pages.route.js
+++ routes/pages.route.js
@@ -11,14 +11,15 @@ router.use(cookieParser(config.cookie))
 router.get('/', (req, res) => {
     res.render('index', { title: 'Bagsfriends.ru' })
 })
-router.get('/shop', async(req, res) => {
+router.get('/shop', async (req, res) => {
     const products = await Product.find({ visible: true }).lean()
     console.log(products);
     res.render('./pages/public/shop', { title: 'Каталог', products })
 })
 
-router.get('/shop/product/:id', async(req, res) => {
+router.get('/shop/product/:id', async (req, res) => {
     const product = await Product.findById(req.params.id).lean()
+    console.log('prodid', product);
     res.render('./pages/public/productPage', { title: product.prodTitle, product })
 })
 
@@ -26,7 +27,7 @@ router.get('/info', (req, res) => {
     res.render('./pages/public/info', { title: 'Условия' })
 })
 
-router.get('/blog', async(req, res) => {
+router.get('/blog', async (req, res) => {
     const news = await News.find({ visible: true }).lean()
     res.render('./pages/public/blog', { title: 'Новости мастерской', news })
 })
@@ -35,28 +36,30 @@ router.get('/contact', (req, res) => {
     res.render('./pages/public/contact', { title: 'Контакты' })
 })
 
-router.get('/basket', async(req, res) => {
+router.get('/basket', async (req, res) => {
     console.log(req.cookies.order)
     if (req.cookies.order) {
         products = req.cookies.order.split(' ')
-        console.log(products)
+        console.log('prode', products)
 
         var basket = [];
 
-
-        /*for (let i = 0; i < products.lenght; i++) {
-            product = Product.findById(products[i].lean())
+        for (let i = 0; i < products.length; i++) {
+            console.log('id', products[i]);
+            product = await Product.findById(products[i]).lean()
+            console.log('prodes', product)
             basket.push(product)
-        }*/
-        await products.forEach(element => {
+        }
+        /*await products.forEach(element => {
             product = Product.findById(element).lean()
+            console.log('prod', product);
             basket.push(product)
-        });
+        });*/
 
-        console.log('basket', basket[0]);
+        console.log('basket', basket);
         res.render('./pages/public/basket', { title: 'Корзина', basket })
-    }
-    res.render('./pages/public/basket', { title: 'Корзина' })
+    } else
+        res.render('./pages/public/basket', { title: 'Корзина' })
 })
 
 module.exports = router

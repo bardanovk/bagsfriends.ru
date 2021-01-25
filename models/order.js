157f47f5a26b04d77ea543a471fb59d8ba38cd68
bardanovk <bardanovk@gmail.com>
2021-01-25 08:00:00 +0300

    create order's model

--- models/order.js
+++ models/order.js
@@ -1,7 +1,16 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model, Types } = require('mongoose')
 
 const schema = new Schema({
 
+    orderNumber: { type: String, required: true, unique: true },
+    basket: [{ type: Types.ObjectId, required: true }],
+    status: { type: String, required: true, default: 'Ожидается подтверждение' },
+    totalPrice: { type: String, required: true },
+    customer: { type: String, required: true },
+    phone: { type: String, required: true },
+
+    address: { type: String, required: true }
+
 })
 
-module.exports = model('Order')
+module.exports = model('Order', schema)

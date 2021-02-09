698103da1646a48bfa0170405857b3f8f6cc37da
bardanovk <bardanovk@gmail.com>
2021-02-10 02:55:26 +0300

    add manageStyle.scss

--- middleware/sassRender.js
+++ middleware/sassRender.js
@@ -12,7 +12,20 @@ const render = async() => {
         else {
             fs.writeFile('./static/style/style.css', result.css.toString(), (err) => {
                 if (err) throw err;
-                console.log('Sass compiled!')
+                console.log('General sass compiled!')
+            })
+        }
+    })
+
+    await sass.render({
+        file: './sass/manageStyle.scss'
+    }, (error, result) => {
+        if (error)
+            console.log('SASS ERROR', error)
+        else {
+            fs.writeFile('./static/style/manageStyle.css', result.css.toString(), (err) => {
+                if (err) throw err;
+                console.log('Manage sass compiled!')
             })
         }
     })

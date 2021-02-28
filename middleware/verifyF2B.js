1f3f81268e0b9f1bbcba31056fb50a1681db3843
bardanovk <bardanovk@gmail.com>
2021-02-28 11:02:11 +0300

    f2b error fix

--- middleware/verifyF2B.js
+++ middleware/verifyF2B.js
@@ -3,19 +3,8 @@ const config = require('../config/tokens')
 
 const verify = (token1, token2) => {
     try {
-        if (
-            speakeasy.totp.verify({
-                secret: config.auth1,
-                encoding: 'base32',
-                token: token1
-            }) &&
-            speakeasy.totp.verify({
-                secret: config.auth2,
-                encoding: 'base32',
-                token: token2
-            })) {
-            return true
-        }
+        return speakeasy.totp.verify({ secret: config.auth1, encoding: 'base32', token: token1, window: 2 }) &&
+            speakeasy.totp.verify({ secret: config.auth2, encoding: 'base32', token: token2, window: 2 })
     } catch (error) {
         console.log('VERIFY COOKIE ERROR', error)
         return false

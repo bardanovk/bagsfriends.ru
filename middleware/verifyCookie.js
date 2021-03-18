92a089037e9f7e0c92252726076cd80e7ec7ca32
bardanovk <bardanovk@gmail.com>
2021-03-19 01:43:41 +0300

    update cookies every time when user do some

--- middleware/verifyCookie.js
+++ middleware/verifyCookie.js
@@ -1,19 +1,44 @@
 const bcrypt = require('bcrypt')
 const salt = require('../config/tokens').bcryptSalt
+const hashCookie = require('../middleware/hashCookie')
+const options = require('../config/options')
 
-module.exports = async(req, cookieHash) => {
+// module.exports = async(req, cookieHash) => {
+//     try {
+//         if (req || cookieHash)
+//             return false
+//         const str = 'admin' + '%%' +
+//             JSON.stringify(req.headers['user-agent']).replace(/"/g, '') + '%%' +
+//             JSON.stringify(req.headers['accept']).replace(/"/g, '') + '%%' +
+//             JSON.stringify(req.headers['x-forwarded-for'] || req.connection.remoteAddress).replace(/"/g, '') + '%%' + JSON.stringify(salt).replace(/"/g, '')
+//             //console.log('str', str)
+
+//         return await bcrypt.compare(str, cookieHash)
+//     } catch (e) {
+//         console.log('VERIFY COOKIE ERROR', e)
+//         return false
+//     }
+// }
+
+module.exports = async(req, res) => {
     try {
-        if (req || cookieHash)
+        // console.log(req.signedCookies.sid);
+        if (req.signedCookies.sid === undefined)
             return false
+                // console.log(typeof(req.signedCookies.sid))
         const str = 'admin' + '%%' +
             JSON.stringify(req.headers['user-agent']).replace(/"/g, '') + '%%' +
             JSON.stringify(req.headers['accept']).replace(/"/g, '') + '%%' +
             JSON.stringify(req.headers['x-forwarded-for'] || req.connection.remoteAddress).replace(/"/g, '') + '%%' + JSON.stringify(salt).replace(/"/g, '')
-            //console.log('str', str)
-
-        return await bcrypt.compare(str, cookieHash)
+        if (await bcrypt.compare(str, req.signedCookies.sid)) {
+            res.cookie('sid', await hashCookie(req), options.admCookieOptions)
+            return true
+        } else {
+            res.clearCookie('sid')
+            return false
+        }
     } catch (e) {
-        console.log('VERIFY COOKIE ERROR', e)
+        console.log('VERIFY COOKIE ERROR:', e)
         return false
     }
 }

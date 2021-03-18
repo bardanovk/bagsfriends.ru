16a5159132aca2375811218e401cefe22ab11cdf
bardanovk <bardanovk@gmail.com>
2021-03-19 01:38:15 +0300

    new design blog page

--- middleware/dateParser.js
+++ middleware/dateParser.js
@@ -1,50 +1,50 @@
 module.exports = (dbDate) => {
-    let tempDate = String(dbDate).split(' ')
+    //function dateparse(dbDate) {
+    const ISODate = new Date(dbDate).toISOString().substring(0, 10)
 
+    tempDate = String(ISODate).split('-')
 
-    let day = Number(tempDate[2])
     let month
-
     switch (tempDate[1]) {
-        case 'Jan':
+        case '01':
             month = 'Января';
             break;
-        case 'Feb':
+        case '02':
             month = 'Февраля';
             break;
-        case 'Mar':
+        case '03':
             month = 'Марта';
             break;
-        case 'Apr':
+        case '04':
             month = 'Апреля';
             break;
-        case 'May':
+        case '05':
             month = 'Мая';
             break;
-        case 'Jun':
+        case '06':
             month = 'Июня';
             break;
-        case 'Jul':
+        case '07':
             month = 'Июля';
             break;
-        case 'Aug':
+        case '08':
             month = 'Августа';
             break;
-        case 'Sep':
+        case '09':
             month = 'Сентября';
             break;
-        case 'Oct':
+        case '10':
             month = 'Октября';
             break;
-        case 'Nov':
+        case '11':
             month = 'Ноября';
             break;
-        case 'Dec':
+        case '12':
             month = 'Декабря';
             break;
         default:
             break;
     }
 
-    return '' + day + ' ' + month + ' ' + tempDate[3] + ' г.'
+    return '' + tempDate[2] + ' ' + month + ' ' + tempDate[0] + ' г.'
 }

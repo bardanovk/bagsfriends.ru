4149554cc9552e828a32ff2a79db3f9b77cf9800
bardanovk <bardanovk@gmail.com>
2021-03-19 01:51:17 +0300

    add string date

--- models/news.js
+++ models/news.js
@@ -1,10 +1,12 @@
 const { Schema, model } = require('mongoose')
+const dateParse = require('../middleware/dateParser')
 
 const schema = new Schema({
     newsTitle: { type: String, required: true, },
     category: { type: Array, required: true },
     text: { type: String },
     date: { type: Date, default: Date.now() },
+    newsDate: { type: String, default: dateParse(Date.now()) },
     discription: { type: String },
     visible: { type: Boolean },
     views: { type: Number, default: 0 }

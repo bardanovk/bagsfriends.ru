d58876e8ad99c99b8b6a72f63b1436c283756a2e
bardanovk <bardanovk@gmail.com>
2021-03-19 01:40:11 +0300

    navbar animation

--- static/script/preloader.js
+++ static/script/preloader.js
@@ -1,3 +1,43 @@
+let arr
+let arrl
 window.onload = () => {
     document.querySelector('body').style = "display: block"
+    arr = document.querySelectorAll('.navbar-buttons-button')
+    arrl = document.querySelectorAll('.navbar-link')
 }
+
+// window.onload = () => {
+//     let arr = document.querySelectorAll('.navbar-buttons-button')
+// }
+
+window.addEventListener('scroll', () => {
+
+    if (document.documentElement.scrollTop !== 0 || window.pageYOffset !== 0) {
+        document.getElementById('header').classList.add('header-minimal')
+        document.getElementById('header-logo').classList.add('header-minimal-logo')
+        arrl.forEach(element => {
+            element.classList.add('navbar-links-minimal')
+        });
+
+        arr.forEach(element => {
+            element.classList.add('header-buttons-minimal')
+        });
+        // // arr.forEach(element => {
+        //     element.classList.add('header-buttons-minimal')
+        // });
+    } else {
+        document.getElementById('header').classList.remove('header-minimal')
+        document.getElementById('header-logo').classList.remove('header-minimal-logo')
+        arr.forEach(element => {
+            element.classList.remove('header-buttons-minimal')
+        });
+        arrl.forEach(element => {
+            element.classList.remove('navbar-links-minimal')
+        });
+        document.querySelector('.navbar-link').classList.remove('navbar-links-minimal')
+    }
+
+
+
+
+})

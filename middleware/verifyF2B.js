ccdf1f081a0a84ced9bf6706952d2dacdfdf3bfa
bardanovk <bardanovk@gmail.com>
2021-06-04 17:54:42 +0300

    a

--- middleware/verifyF2B.js
+++ middleware/verifyF2B.js
@@ -3,7 +3,9 @@ const config = require('../config/tokens')
 
 const verify = (token1, token2) => {
     try {
-        //console.log('tokens', token1, token2);
+        //console.log('tokens', token1, token2);3
+        console.log(speakeasy.totp.verify({ secret: config.auth1, encoding: 'base32', token: token1, window: 2 }) &&
+            speakeasy.totp.verify({ secret: config.auth2, encoding: 'base32', token: token2, window: 2 }));
         return speakeasy.totp.verify({ secret: config.auth1, encoding: 'base32', token: token1, window: 2 }) &&
             speakeasy.totp.verify({ secret: config.auth2, encoding: 'base32', token: token2, window: 2 })
     } catch (e) {
